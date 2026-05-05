44e1542748a0f320a26c7c9316e92e386e1b0450
Hugol4ka <hugolaca12300@gmail.com>
2026-05-05 16:15:22 +0200

    Add script for print_comb2.py

--- python_fundamentals/control_flow/print_comb2.py
+++ python_fundamentals/control_flow/print_comb2.py
@@ -4,5 +4,4 @@ for i in range(100):
     if i < 99:
         print("{:02d}, ".format(i), end="")
     else:
-        # Affichage du dernier nombre SEUL (le saut de ligne est automatique ici)
         print("{:02d}".format(i))
