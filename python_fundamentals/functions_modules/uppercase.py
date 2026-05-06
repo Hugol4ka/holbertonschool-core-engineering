5ca4b8826715e9f8cd0bab9338083c7b10cea699
Hugol4ka <hugolaca12300@gmail.com>
2026-05-06 12:23:34 +0200

    Refactor uppercase function to streamline character case conversion logic

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -4,10 +4,12 @@
 
 def uppercase(str):
     """print the string in uppercase followed by a new line."""
+
+    if not isinstance(str, str):
+        str = "{}".format(str)
+
     for char in str:
         if ord(char) >= 97 and ord(char) <= 122:
-            ascii_code = ord(char)
-            upper_code = ascii_code - 32
-            char = chr(upper_code)
+            char = chr(ord(char) - 32)
         print("{}".format(char), end="")
     print("")
