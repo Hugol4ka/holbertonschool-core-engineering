b8ff40136e3a478f6bc222cd443b7cb698d52ddd
Hugol4ka <hugolaca12300@gmail.com>
2026-05-11 13:54:58 +0200

    Fix typo in module docstring for Square class

--- python_oop/classes_and_object_model/4-square.py
+++ python_oop/classes_and_object_model/4-square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""This module defines an Square class."""
+"""This module defines a Square class."""
 
 
 class Square:
     """A class that defines a square by its size."""
+
     def __init__(self, size=0):
         """Initialize the square with a private size attribute."""
-
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
