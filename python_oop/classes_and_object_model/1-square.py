fb4c0ed2557de0011387d7fb9305ec3703c59ca3
Hugol4ka <hugolaca12300@gmail.com>
2026-05-11 12:48:40 +0200

    Fix typo in module docstring and update Square class initialization method to include size validation

--- python_oop/classes_and_object_model/1-square.py
+++ python_oop/classes_and_object_model/1-square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-"""This module defines a Square class."""
+"""This module defines an Square class."""
 
 
 class Square:
     """A class that defines a square by its size."""
+    def __init__(self, size=0):
+        """Initialize the square with a private size attribute."""
 
-
-def __init__(self, size):
-    """Initialize the square with a private size attribute."""
-    self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
