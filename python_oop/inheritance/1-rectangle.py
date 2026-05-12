7c8e8ead6ba13b67eb5dcccacc73e8b181986c35
Hugol4ka <hugolaca12300@gmail.com>
2026-05-12 15:32:59 +0200

    Remove BaseGeometry class definition from rectangle.py and import from base_geometry module

--- python_oop/inheritance/1-rectangle.py
+++ python_oop/inheritance/1-rectangle.py
@@ -1,20 +1,6 @@
 #!/usr/bin/env python3
 """Module defining the BaseGeometry class"""
-
-
-class BaseGeometry:
-    """A foundational class for geometric shapes"""
-
-    def area(self):
-        """Raises an Exception because area is not implemented yet"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates that value is a positive integer"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
