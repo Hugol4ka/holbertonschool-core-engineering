f83d853a6599e914e0c5a880382630226dbf0be5
Hugol4ka <hugolaca12300@gmail.com>
2026-05-12 13:12:41 +0200

    Add area and perimeter methods to Rectangle class for geometric calculations

--- python_oop/classes_and_object_model/1-rectangle.py
+++ python_oop/classes_and_object_model/1-rectangle.py
@@ -37,3 +37,13 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        """Returns the rectangle area."""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """Returns the rectangle perimeter."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__height + self.__width)
