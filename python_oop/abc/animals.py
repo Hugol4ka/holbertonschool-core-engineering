5384306de0d0c68ecd83e4eb2e6215f56b5f716f
Hugol4ka <hugolaca12300@gmail.com>
2026-05-13 12:00:30 +0200

    Add Dog and Cat classes inheriting from Animal with sound methods

--- python_oop/abc/animals.py
+++ python_oop/abc/animals.py
@@ -3,6 +3,22 @@ from abc import ABC, abstractmethod
 
 
 class Animal(ABC):
+    """Abstract class for all animals"""
     @abstractmethod
     def sound(self):
+        """Abstract method that subclasses MUST implement"""
         pass
+
+class Dog(Animal):
+    """Dog class inheriting from Animal"""
+    
+    def sound(self):
+        """Returns the specific sound of a dog"""
+        return "Bark"
+    
+class Cat(Animal):
+    """Cat class inheriting from Animal"""
+
+    def sound(self):
+        """Returns the specific sound of a cat"""
+        return "Meow"
