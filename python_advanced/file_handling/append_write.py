47a05febd9a2f930bb5893ab85c0a32a608f3232
Hugol4ka <hugolaca12300@gmail.com>
2026-05-25 16:51:38 +0200

    Fix docstring formatting in append_write function for clarity

--- python_advanced/file_handling/append_write.py
+++ python_advanced/file_handling/append_write.py
@@ -3,6 +3,7 @@
 
 
 def append_write(filename="", text=""):
-    """Appends a string at the end of a UTF-8 text file and returns characters added."""
+    """Appends a string at the end of a UTF-8 text file
+    and returns characters added."""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
